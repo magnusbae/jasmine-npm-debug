8e7f32ec76dc0e02597fb9367d734b337adb6917
Magnus Bae <magnusbae@gmail.com>
2015-09-16 10:58:45 +0200

    Fixed and tested with jasmine-npm
    Chrome debugging performance was quite slow compared to WebStorm using ```node --debug-brk node_modules/jasmine/bin/jasmine.js```

--- cli.js
+++ cli.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var DebugServer = require('node-inspector/lib/debug-server').DebugServer;
-var config = require('node-inspector/lib/config');
 var spawn = require('child_process').spawn;
 var open = require('open');
 var fs = require('fs');
@@ -12,6 +11,15 @@ debugServer.on('close', function () {
   process.exit();
 });
 
+var config = {
+  "webPort": 8082,
+  "webHost": null,
+  "debugPort": 5858,
+  "saveLiveEdit": false,
+  "hidden": []
+};
+
+
 debugServer.start(config);
 
 var args = [
@@ -21,8 +29,6 @@ var args = [
 
 if (process.argv.length > 2) {
   args.push(process.argv.slice(2));
-} else {
-  args.push('spec/index.spec.js');
 }
 
 args.forEach(function(arg) {
